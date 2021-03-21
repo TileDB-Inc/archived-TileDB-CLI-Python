8ede89613f10c488552b57c4818bbb802db20704
Vivian Nguyen <vivian@tiledb.com>
2021-03-21 16:23:35 -0500

    Addition of Various Flags to convert_from csv

--- commands/convert_from.py
+++ commands/convert_from.py
@@ -2,16 +2,198 @@ import tiledb
 
 import click
 
+
 @click.group()
 def convert_from():
     pass
 
 
+class OptionalInt(click.ParamType):
+    name = "optional_int"
+
+    def convert(self, value, param, ctx):
+        try:
+            return int(value)
+        except ValueError:
+            self.fail(f"{value!r} is not a valid integer", param, ctx)
+
+
+class TileSpecifier(click.ParamType):
+    name = "tile_specifier"
+
+    def convert(self, value, param, ctx):
+        values = str(value).split(":")
+        if len(values) == 1:
+            try:
+                return int(values[0])
+            except ValueError:
+                self.fail(f"{values[0]!r} is not a valid integer", param, ctx)
+        elif len(values) == 2:
+            try:
+                return (values[0], int(values[1]))
+            except ValueError:
+                self.fail(f"{values[0]!r} is not a valid integer", param, ctx)
+        else:
+            self.fail(f"Too many arguments provided", param, ctx)
+
+
 @click.command()
 @click.argument("csv_file")
 @click.argument("uri")
-def csv(csv_file, uri):
-    tiledb.from_csv(uri, csv_file)
+@click.option(
+    "--allows-duplicates/--no-duplicates",
+    help=(
+        "Generated schema should allow duplicates. "
+        "By default, duplciates are allowed"
+    ),
+    default=True,
+)
+@click.option(
+    "--capacity",
+    "-c",
+    metavar="<int>",
+    help=("Schema capacity. By default, 0, which uses the libtiledb internal default"),
+    type=int,
+    default=0,
+)
+@click.option(
+    "--cell-order",
+    "-C",
+    metavar="(row-major | column-major | global)",
+    help=("Specify the cell ordering. By default, row-major"),
+    type=click.Choice(["row-major", "column-major", "global"], case_sensitive=True),
+    default="row-major",
+)
+@click.option(
+    "--full-domain/--limited-domain",
+    help=(
+        "Specify whether dimensions should be created with full range of the dtype. "
+        "By default, the limited-domain ranges from the min and max values of "
+        "the ingested CSV file"
+    ),
+    default=False,
+)
+@click.option(
+    "--date-spec",
+    "-d",
+    metavar="<column> <datetime format spec>",
+    help=(
+        "Apply a datetime format spec to the column. Format must be specified "
+        "using the Python format codes: "
+        "\n"
+        "\thttps://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior"
+        "\n"
+        "Multiple columns may be provided by passing the flag multiple times "
+        "(e.g. '-d col1 %d/%m/%y -d col2 %A %d. %B %Y')"
+    ),
+    multiple=True,
+    nargs=2,
+    type=str,
+)
+@click.option(
+    "--mode",
+    "-m",
+    metavar="(ingest | schema_only | append)",
+    help=("Specify the ingestion mode. By default, ingest"),
+    type=click.Choice(["ingest", "schema_only", "append"], case_sensitive=True),
+    default="ingest",
+)
+@click.option(
+    "--row-start-idx",
+    "-r",
+    metavar="<int>",
+    help=("Start index to start new write (for row-indexed ingestions). By default, 0"),
+    type=int,
+    default=0,
+)
+@click.option(
+    "--sparse/--dense",
+    help=(
+        "Specify whether the resulting array should be sparse or dense. "
+        "By default, sparse"
+    ),
+    default=True,
+)
+@click.option(
+    "--tile",
+    "-t",
+    metavar="<int> | <column name>:<int>",
+    help=(
+        "Providing a single <int> will apply the tiling to each dimension. "
+        "Assign tiling to a specific dimension by providing the <column name>:<int>. "
+        "Alternatively, assign tilling to each dimension by passing the flag "
+        "multiple times (e.g. '-t dim_x:5 -t dim_y:8')"
+    ),
+    multiple=True,
+    type=TileSpecifier(),
+    default=(),
+)
+@click.option(
+    "--tile-order",
+    "-T",
+    metavar="ingest | schema_only | append",
+    help=("Specify the tile ordering. By default, row-major"),
+    type=click.Choice(["row-major", "column-major", "global"], case_sensitive=True),
+    default="row-major",
+)
+@click.option(
+    "--timestamp",
+    "-u",
+    metavar="<int>",
+    help=(
+        "Write TileDB array at specific UNIX timestamp. "
+        "By default, the current system time"
+    ),
+    type=OptionalInt(),
+    default=None,
+)
+def csv(
+    csv_file,
+    uri,
+    # tiledb keyword args
+    allows_duplicates,
+    capacity,
+    cell_order,
+    date_spec,
+    full_domain,
+    mode,
+    row_start_idx,
+    sparse,
+    tile,
+    tile_order,
+    timestamp,
+    # pandas keyword args
+):
+    """
+    Convert a csv_file into a TileDB array located at uri.
+    """
+    if tile:
+        if len(tile) == 1:
+            tile = tile[0]
+        elif len(tile) > 1:
+            if any(isinstance(t, int) for t in tile):
+                raise click.BadOptionUsage(
+                    "The --tile/-t flag can only be used once if using an <int> argument. "
+                    "Multiple uses of the flag require <column name> <int> arguments."
+                )
+            tile = dict(tile)
+
+    tiledb.from_csv(
+        uri,
+        csv_file,
+        # tiledb keyword args
+        allows_duplicates=allows_duplicates,
+        capacity=capacity,
+        date_spec=dict(date_spec) if date_spec else None,
+        cell_order=cell_order,
+        full_domain=full_domain,
+        mode=mode,
+        row_start_idx=row_start_idx,
+        sparse=sparse,
+        tile_order=tile_order,
+        tile=tile if tile else None,
+        timestamp=timestamp,
+    )
 
 
 convert_from.add_command(csv)
--- tests/test_convert_from.py
+++ tests/test_convert_from.py
@@ -16,12 +16,12 @@ def create_test_simple_csv(temp_rootdir):
     with open(path, mode="w") as csv_input:
         csv_input.write(
             (
-                "a,b,c\n"
-                '1,"text",3.4\n'
-                '2,"hello",1.234\n'
-                '3,"goodbye",111.232\n'
-                '4,"world",123123.12\n'
-                '10,"raisins",14.232\n'
+                "a,b,c,date\n"
+                '1,"text",3.4,Mar/02/2021\n'
+                '2,"hello",1.234,Apr/07/2021\n'
+                '3,"goodbye",111.232,Dec/17/2021\n'
+                '4,"world",123123.12,Jul/21/2021\n'
+                '10,"raisins",14.232,Nov/09/2021\n'
             )
         )
 
@@ -33,18 +33,262 @@ class TestCSV:
 
             tiledb convert_from [csv_file] [uri]
         """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test.tdb")
+
         runner = CliRunner()
         result = runner.invoke(
             root,
             [
                 "convert-from",
                 "csv",
-                os.path.join(temp_rootdir, "simple.csv"),
-                os.path.join(temp_rootdir, "simple.tdb"),
+                input_path,
+                uri,
             ],
         )
 
-        print(result)
-        print(result.stdout)
+        assert result.exit_code == 0
+
+        with tiledb.open(uri) as array:
+            print(array[:])
+
+    def test_sparse(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] --sparse
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_sparse.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "--sparse"],
+        )
+
+        assert result.exit_code == 0
+
+        with tiledb.open(uri) as array:
+            assert array.schema.sparse == True
+
+    def test_dense(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] --dense
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_dense.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "--dense"],
+        )
+
+        assert result.exit_code == 0
+
+        with tiledb.open(uri) as array:
+            assert array.schema.sparse == False
+
+    def test_allows_duplicates(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] --allows-duplicates
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_allows_duplicates.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "--allows-duplicates"],
+        )
+
+        assert result.exit_code == 0
+
+    def test_capacity(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] -c <int>
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_capacity.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-c", 1],
+        )
+
+        assert result.exit_code == 0
+
+    def test_cell_order(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] -C (row-major | column-major | global)
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_cell_order.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-C", "global"],
+        )
+
+        assert result.exit_code == 0
+
+    def test_full_domain(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] --full-domain
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_full_domain.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "--full-domain"],
+        )
+
+        assert result.exit_code == 0
+
+    def test_date_spec(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] --date-spec <column> <datetime format spec>
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_date_spec.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-d", "date", "%b/%d/%Y"],
+        )
+
+        assert result.exit_code == 0
+
+    def test_mode(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] -m (ingest | schema_only | append)
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_mode.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-m", "schema_only"],
+        )
+
+        assert result.exit_code == 0
+
+    def test_row_start_idx(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] -r <int>
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_row_start_idx.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-r", 5],
+        )
+
+        assert result.exit_code == 0
+
+    def test_row_order(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] -C (row-major | column-major | global)
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_row_order.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-C", "global"],
+        )
+
+        assert result.exit_code == 0
+
+    def test_tile_int(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] -t <int>
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_tile_int.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-t", 3],
+        )
+
+        assert result.exit_code == 0
+
+    def test_tile_column_name(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] -t <column name> <int>
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_tile_column_name.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-t", "a:3", "-t", "b:2"],
+        )
+
+        assert result.exit_code == 0
+
+    def test_timestamp(self, temp_rootdir):
+        """
+        Test for command
+
+            tiledb convert_from [csv_file] [uri] -u <int>
+        """
+        test_name = "simple"
+        input_path = os.path.join(temp_rootdir, f"{test_name}.csv")
+        uri = os.path.join(temp_rootdir, "test_timestamp.tdb")
+
+        runner = CliRunner()
+        result = runner.invoke(
+            root,
+            ["convert-from", "csv", input_path, uri, "-u", 3],
+        )
 
         assert result.exit_code == 0
