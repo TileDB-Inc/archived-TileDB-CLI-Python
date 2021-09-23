0f59ec3695f5bc5645fe9f7907a33f6f3bbd3c84
Vivian Nguyen <vivian@tiledb.com>
2021-09-23 18:56:40 -0500

    Refactor `tiledb dump array` Command
    * Uses query()
    * Test all atrribute and dimension types

--- commands/dump.py
+++ commands/dump.py
@@ -1,7 +1,10 @@
+from numpy.lib.function_base import i0
 import tiledb
 
 import click
 import pprint
+import re
+import sys
 
 
 @click.group()
@@ -54,6 +57,7 @@ def schema(uri):
 
 @click.command()
 @click.argument("uri")
+@click.argument("selection", nargs=-1)
 @click.option(
     "--attribute",
     "-A",
@@ -61,23 +65,22 @@ def schema(uri):
     help=(
         "Output data from given attribute. Multiple attributes may be provided "
         "by passing the flag multiple times e.g. '-A attr1 -A attr2'. "
-        "By default, all attributes are output"
+        "By default, all attributes are selected"
     ),
     multiple=True,
     default=[],
 )
 @click.option(
-    "--rows",
-    "-n",
-    metavar="<start int> <end int>",
+    "--dimension",
+    "-d",
+    metavar="<str>",
     help=(
-        "Output data from the range of rows beginning with <start int> and "
-        "ending with <end int>. By default, '-n 1 5' which outputs the first "
-        "five rows"
+        "Output data from given dimension. Multiple dimensions may be provided "
+        "by passing the flag multiple times e.g. '-d dim1 -d dim2'. "
+        "By default, all dimensions are selected"
     ),
-    nargs=2,
-    type=int,
-    default=(1, 5),
+    multiple=True,
+    default=[],
 )
 @click.option(
     "--timestamp",
@@ -87,32 +90,84 @@ def schema(uri):
     type=int,
     default=None,
 )
-def array(uri, attribute, rows, timestamp):
+def array(uri, selection, attribute, dimension, timestamp):
     """
-    Output the data of a TileDB array located at uri.
+    Output the data of a TileDB array located at uri with a given selection.
+    The selection is given per dimension and is a scalar or slice that matches
+    the type of the underlying dimension.
+
+    For datetime dimensions, select by enclose the datetime string in quotes.
+    e.g. tiledb dump array uri_to_array '5-3-2020 12:00':"5-3-2021"
+
+    Example:
+        tiledb dump schema tiledb://TileDB-Inc/quickstart_sparse
+        #
+        # This array has two dimensions - rows and cols.
+        #
+        # ArraySchema(
+        # domain=Domain(*[
+        #     Dim(name='rows', domain=(1, 4), tile='4', dtype='int32'),
+        #     Dim(name='cols', domain=(1, 4), tile='4', dtype='int32'),
+        # ]),
+        # ...
+
+        tiledb dump array tiledb://TileDB-Inc/quickstart_sparse 2 3:4
+        #
+        # Read row 2 and columns 3 thru 4.
+        #    rows  cols  a
+        # 0     2     3  3
+        # 1     2     4  2
+
+        tiledb dump array tiledb://TileDB-Inc/quickstart_sparse 2 -d rows
+        #
+        # Select only "rows" dimension. Read row 2.
+        #    rows  a
+        # 0     2  3
+        # 1     2  2
     """
+    import numpy as np
+
     with tiledb.open(uri, timestamp=timestamp) as array:
         pp = pprint.PrettyPrinter()
 
-        if rows[0] <= 0 or rows[1] <= 0:
-            raise click.BadOptionUsage(
-                option_name="rows",
-                message="The arguments to --rows/-n needs to be positive integers.",
+        attrs = None if attribute == () else attribute
+        sels = list(selection)
+        dims = (
+            [array.domain.dim(i).name for i in range(array.domain.ndim)]
+            if dimension == ()
+            else dimension
+        )
+
+        if len(dims) != len(sels):
+            click.echo(
+                f"Error: The number of selections ({len(sels)}) needs to match "
+                f"the number of dimensions ({len(dims)})",
+                err=True,
             )
+            sys.exit(1)
 
-        subarray = array[rows[0] : rows[1] + 1]
+        for i, dim in enumerate(dims):
+            dt = np.dtype(array.domain.dim(dim).dtype)
+            if np.issubdtype(dt, np.datetime64):
+                quoted = re.compile("'[^']*'|\"[^\"]*\"")
+                sel = [p[1:-1] if p else None for p in quoted.findall(sels[i])]
+                if len(sel) == 0:
+                    click.echo(
+                        "Error: could not parse the selection for the datetime"
+                        f"dimension '{dim}'. (Did you enclose the selection in "
+                        "quotes?)",
+                        err=True,
+                    )
+                sel = np.array(sel, dtype=dt)
+                sels[i] = slice(*sel) if len(sel) > 1 else sel[0]
+            else:
+                sel = [p if p else None for p in sels[i].split(":")]
+                sel = np.array(sel, dtype=dt)
+                sels[i] = slice(*sel) if ":" in sels[i] else sel[0]
 
-        if not array.schema.sparse and not array.schema.attr(0).name:
-            click.echo(pp.pformat({"": subarray}))
-        else:
-            if not attribute:
-                attribute = [
-                    array.schema.attr(n).name for n in range(array.schema.nattr)
-                ]
-            filtered_subarray = {
-                key: value for (key, value) in subarray.items() if key in attribute
-            }
-            click.echo(pp.pformat(filtered_subarray))
+        query = array.query(attrs=attrs, dims=dims, use_arrow=False)
+        subarray = query[tuple(sels)]
+        click.echo(pp.pformat(subarray))
 
 
 @click.command()
