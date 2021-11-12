c9a01905169676faf32a8e7d75a404072f6b9363
Vivian Nguyen <vivian@tiledb.com>
2021-11-12 13:31:59 -0600

    Addition of `delete-fragments` Command (#21)
    * Add `delete-fragments` command
    * Change Required Version of TileDB-Py to 0.10.5

--- commands/root.py
+++ commands/root.py
@@ -1,7 +1,7 @@
 import click
 
 from .cloud import cloud
-from .consolidate_and_vacuum import consolidate, vacuum
+from .consolidate_and_vacuum import consolidate, vacuum, delete_fragments
 from .convert_from import convert_from
 from .dump import dump
 
@@ -11,8 +11,12 @@ def root():
     pass
 
 
+# groups
 root.add_command(cloud)
 root.add_command(convert_from)
 root.add_command(consolidate)
 root.add_command(dump)
 root.add_command(vacuum)
+
+# commands
+root.add_command(delete_fragments)
