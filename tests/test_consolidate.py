0ee181e4672ee272696ef34d99c6782c2f3f57e8
Vivian Nguyen <vivian@tiledb.com>
2021-11-15 09:56:06 -0600

    Run `fragments copy` On POSIX only

---
+++ tests/test_consolidate.py
@@ -0,0 +1,100 @@
+import tiledb
+from common import test_array_names
+from commands.root import root
+
+import itertools
+import numpy as np
+import os
+import pytest
+import shutil
+
+
+@pytest.fixture(scope="function")
+def uri(temp_rootdir):
+    """
+    Create a simple dense test array.
+    """
+    path = os.path.abspath(os.path.join(temp_rootdir, "test_array"))
+
+    ctx = tiledb.default_ctx()
+    rows_dim = tiledb.Dim(ctx=ctx, domain=(1, 25), dtype=np.int64)
+    cols_dim = tiledb.Dim(ctx=ctx, domain=(1, 12), dtype=np.int64)
+    dom = tiledb.Domain(rows_dim, cols_dim, ctx=ctx)
+    att1 = tiledb.Attr(name="a", ctx=ctx, dtype=np.float64)
+    att2 = tiledb.Attr(name="b", ctx=ctx, dtype=np.float64)
+    schema = tiledb.ArraySchema(ctx=ctx, domain=dom, attrs=(att1, att2))
+
+    tiledb.DenseArray.create(path, schema)
+
+    data = np.reshape(np.arange(300), (25, 12))
+
+    with tiledb.DenseArray(path, mode="w", timestamp=1) as A:
+        A.meta["meta_int"] = 1
+        A[:] = {"a": data, "b": data}
+
+    with tiledb.DenseArray(path, mode="w", timestamp=2) as A:
+        A.meta["meta_int"] = 2
+        A[:] = {"a": data / 2, "b": data * 2}
+
+    yield path
+
+    shutil.rmtree(path)
+
+
+def test_fragments_consolidate_and_vacuum(runner, uri):
+    """
+    Test for command
+
+        tiledb consolidate fragments [array_uri]
+        tiledb vacuum fragments [array_uri]
+    """
+    fragments = tiledb.array_fragments(uri)
+    assert len(fragments) == 2
+
+    result = runner.invoke(root, ["consolidate", "fragments", uri])
+    assert result.exit_code == 0
+
+    fragments = tiledb.array_fragments(uri)
+    assert len(fragments) == 1
+    assert len(fragments.to_vacuum) == 2
+
+    result = runner.invoke(root, ["vacuum", "fragments", uri])
+    assert result.exit_code == 0
+
+    fragments = tiledb.array_fragments(uri)
+    assert len(fragments) == 1
+    assert len(fragments.to_vacuum) == 0
+
+
+def test_fragment_metadata_consolidate_and_vacuum(runner, uri):
+    """
+    Test for command
+
+        tiledb consolidate fragment-metadata [array_uri]
+        tiledb vacuum fragment-metadata [array_uri]
+    """
+    fragments = tiledb.array_fragments(uri)
+    assert fragments.unconsolidated_metadata_num == 2
+
+    result = runner.invoke(root, ["consolidate", "fragment-metadata", uri])
+    assert result.exit_code == 0
+
+    fragments = tiledb.array_fragments(uri)
+    assert fragments.unconsolidated_metadata_num == 0
+
+    result = runner.invoke(root, ["vacuum", "fragment-metadata", uri])
+    assert result.exit_code == 0
+
+
+def test_array_metadata_consolidate_and_vacuum(runner, uri):
+    """
+    Test for command
+
+        tiledb consolidate array-metadata [array_uri]
+        tiledb vacuum array-metadata [array_uri]
+    """
+    result = runner.invoke(root, ["consolidate", "array-metadata", uri])
+    assert result.exit_code == 0
+
+    result = runner.invoke(root, ["vacuum", "array-metadata", uri])
+    assert result.exit_code == 0
---
+++ tests/test_fragments.py
@@ -0,0 +1,82 @@
+import tiledb
+from common import test_array_names
+from commands.root import root
+
+import itertools
+import numpy as np
+import os
+import pytest
+import shutil
+import sys
+
+
+@pytest.fixture(scope="function")
+def uri(temp_rootdir):
+    """
+    Create a simple dense test array.
+    """
+    path = os.path.abspath(os.path.join(temp_rootdir, "test_array"))
+
+    ctx = tiledb.default_ctx()
+    rows_dim = tiledb.Dim(ctx=ctx, domain=(1, 25), dtype=np.int64)
+    cols_dim = tiledb.Dim(ctx=ctx, domain=(1, 12), dtype=np.int64)
+    dom = tiledb.Domain(rows_dim, cols_dim, ctx=ctx)
+    att1 = tiledb.Attr(name="a", ctx=ctx, dtype=np.float64)
+    att2 = tiledb.Attr(name="b", ctx=ctx, dtype=np.float64)
+    schema = tiledb.ArraySchema(ctx=ctx, domain=dom, attrs=(att1, att2))
+
+    tiledb.DenseArray.create(path, schema)
+
+    data = np.reshape(np.arange(300), (25, 12))
+
+    for ts in range(1, 4):
+        with tiledb.DenseArray(path, mode="w", timestamp=ts) as A:
+            A[:] = {"a": data, "b": data}
+
+    yield path
+
+    shutil.rmtree(path)
+
+
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="VFS.copy() does not run on windows"
+)
+def test_copy_fragments(runner, uri, temp_rootdir):
+    """
+    Test for command
+
+        tiledb fragments copy [old_array_uri] [new_array_uri] [start_time] [end_time]
+    """
+    old_uri = uri
+    new_uri = os.path.abspath(os.path.join(temp_rootdir, "test_copy_fragments_new"))
+
+    result = runner.invoke(
+        root, ["fragments", "copy", "-v", old_uri, new_uri, "2", "3"]
+    )
+    assert result.exit_code == 0
+
+    fragments = tiledb.array_fragments(old_uri)
+    assert len(fragments) == 3
+    assert fragments.timestamp_range == ((1, 1), (2, 2), (3, 3))
+
+    fragments = tiledb.array_fragments(new_uri)
+    assert len(fragments) == 2
+    assert fragments.timestamp_range == ((2, 2), (3, 3))
+
+
+def test_delete_fragments(runner, uri):
+    """
+    Test for command
+
+        tiledb fragments delete [array_uri] [start_time] [end_time]
+    """
+    fragments = tiledb.array_fragments(uri)
+    assert len(fragments) == 3
+    assert fragments.timestamp_range == ((1, 1), (2, 2), (3, 3))
+
+    result = runner.invoke(root, ["fragments", "delete", "-v", uri, "1", "1"])
+    assert result.exit_code == 0
+
+    fragments = tiledb.array_fragments(uri)
+    assert len(fragments) == 2
+    assert fragments.timestamp_range == ((2, 2), (3, 3))
